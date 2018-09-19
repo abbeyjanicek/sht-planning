30a43c28ca6f1594c4b6474cfa3e34065943aa2c
Abbey Janicek <abbey.janicek@gmail.com>
2018-09-19 16:41:30 -0500

    Created basic form on Add Hike page.

--- src/components/AddHikePage/AddHikePage.js
+++ src/components/AddHikePage/AddHikePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Nav from '../Nav/Nav';
+import { USER_ACTIONS } from '../../redux/actions/userActions';
 
 const mapStateToProps = state => ({
     user: state.user,
@@ -9,6 +10,9 @@ const mapStateToProps = state => ({
 
 class AddHikePage extends Component {
 
+  componentDidMount() {
+    this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
+  }
 
   componentDidUpdate() {
     if (!this.props.user.isLoading && this.props.user.userName === null) {
@@ -22,9 +26,21 @@ class AddHikePage extends Component {
         if (this.props.user.userName) {
           content = (
             <div>
-              <p>
+              <h1>
                 Add a Hike
-              </p>
+              </h1>
+              <form>
+              <input type="text" placeholder="start date" name="start_date" />
+              <input type="text" placeholder="end date" name="end_date" />
+              <input type="text" placeholder="starting mile" name="start_mile" />
+              <input type="text" placeholder="ending mile" name="end_mile" />
+              <input type="text" placeholder="start trailhead" name="start_trailhead" />
+              <input type="text" placeholder="end trailhead" name="end_trailhead" />
+              <h3>Campsites</h3>
+              <p>Not sure where to camp? CLICK HERE to visit the campsite review page.</p>
+              <button>Add Campsite</button>
+              <input type="submit" value="submit" />
+            </form>
             </div>
           );
         }
